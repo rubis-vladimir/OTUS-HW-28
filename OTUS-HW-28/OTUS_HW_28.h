28c75042db4fd044da24fc9b446b9880c523a76d
rubis-vladimir <84345727+rubis-vladimir@users.noreply.github.com>
2022-11-29 20:36:35 +0300

    Initial Commit

---
+++ OTUS-HW-28/OTUS_HW_28.h
@@ -0,0 +1,18 @@
+//
+//  OTUS_HW_28.h
+//  OTUS-HW-28
+//
+//  Created by Владимир Рубис on 29.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OTUS_HW_28.
+FOUNDATION_EXPORT double OTUS_HW_28VersionNumber;
+
+//! Project version string for OTUS_HW_28.
+FOUNDATION_EXPORT const unsigned char OTUS_HW_28VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTUS_HW_28/PublicHeader.h>
+
+
