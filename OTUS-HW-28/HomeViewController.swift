28b8767cd2556a05cb93bf03f2035a367c2d1e7c
rubis-vladimir <84345727+rubis-vladimir@users.noreply.github.com>
2022-11-29 20:44:02 +0300

    first commit

---
+++ OTUS-HW-28/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  OTUS-HW-28
+//
+//  Created by Владимир Рубис on 29.11.2022.
+//
+
+import UIKit
+import SnapKit
+
+final class HomeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
